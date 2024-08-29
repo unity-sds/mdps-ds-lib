44fc322c502e94bc469a238285b99db490517284
Wai Phyo <wai.phyo@jpl.nasa.gov>
2024-08-29 15:03:29 -0700

    feat: initial commit

---
+++ uds_lib/stage_in_out/search_collections_cmr.py
@@ -0,0 +1,6 @@
+from uds_lib.stage_in_out.search_collections_abstract import SearchCollectionsAbstract
+
+
+class SearchCollectionsCmr(SearchCollectionsAbstract):
+    def search(self, **kwargs):
+        raise NotImplementedError()
