094792a1c0dca02c3b23f7b4641dd6a20cf308e3
Wai Phyo <wai.phyo@jpl.nasa.gov>
2024-10-28 11:37:39 -0700

    feat: making it mlutithread

--- mdps_ds_lib/stage_in_out/upload_granules_by_complete_catalog_s3.py
+++ mdps_ds_lib/stage_in_out/upload_granules_by_complete_catalog_s3.py
@@ -95,7 +95,7 @@ class UploadItemExecutor(JobExecutorAbstract):
         try:
             current_granule_stac: Item = self.__gc.get_granules_item(each_child)
         except Exception as e:
-            self.__error_list.append({
+            self.__error_list.put({
                 'granule_file': each_child,
                 'error': 'unable to read the stac file',
                 'details': str(e)
@@ -110,14 +110,14 @@ class UploadItemExecutor(JobExecutorAbstract):
                                                            current_granules_dir)  # returns defaultdict(list)
 
             if 'data' not in current_assets:  # this is still ok .coz extract_assets_href is {'data': [url1, url2], ...}
-                self.__error_list.append({
+                self.__error_list.put({
                     'granule_file': each_child,
                     'error': 'missing "data" in assets',
                     'details': current_assets,
                 })
             current_granule_id = str(current_granule_stac.id)
             if current_granule_id in ['', 'NA', None]:
-                self.__error_list.append({
+                self.__error_list.put({
                     'granule_file': each_child,
                     'error': 'invalid current_granule_id in granule',
                     'details': current_granule_id,
@@ -126,26 +126,21 @@ class UploadItemExecutor(JobExecutorAbstract):
             for asset_type, asset_hrefs in current_assets.items():
                 for asset_name, asset_href in asset_hrefs.items():
                     if not FileUtils.file_exist(asset_href):
-                        self.__error_list.append({
+                        self.__error_list.put({
                             'granule_file': each_child,
                             'error': f'missing uploading file for {asset_type} - {asset_name}',
                             'details': asset_href,
                         })
-                    self.__result_list.append({
+                    self.__result_list.put({
                         'granule_file': each_child,
                         's3_url': f'{self.__staging_bucket}://{current_collection_id}/{current_collection_id}:{current_granule_id}/{os.path.basename(asset_href)}'
                     })
-            current_granule_stac.id = current_granule_id
-            current_granule_stac.collection_id = current_collection_id
-            current_granule_stac.id = f'{current_collection_id}:{current_granule_id}'
-            # self.__result_list.append(current_granule_stac.to_dict(False, False))
         except Exception as e:
-            self.__error_list.append({
+            self.__error_list.put({
                 'granule_file': each_child,
                 'error': 'unexpected error',
                 'details': str(e),
             })
-            current_granule_stac.properties['upload_error'] = str(e)
             LOGGER.exception(f'error while processing: {each_child}')
         return True
 
@@ -217,6 +212,9 @@ class UploadGranulesByCompleteCatalogS3(UploadGranulesAbstract):
         return json.dumps(catalog_json)
 
     def __exec_dry_run(self):
+        local_items = Manager().Queue()
+        error_list = Manager().Queue()
+
         errors = []
         results = []
         if self.OUTPUT_DIRECTORY not in os.environ:
@@ -231,9 +229,26 @@ class UploadGranulesByCompleteCatalogS3(UploadGranulesAbstract):
                 })
         catalog_file_path = os.environ.get(self.CATALOG_FILE)
         child_links = self.__gc.get_child_link_hrefs(catalog_file_path)
-        project_venue_set = (self._project, self._venue)
+
+        job_manager_props = JobManagerProps()
         for each_child in child_links:
-            UploadItemExecutor(results, errors, project_venue_set, self._staging_bucket, self._retry_wait_time_sec, self._retry_times, self._delete_files, True).execute_job(each_child, None)
+            job_manager_props.memory_job_dict[each_child] = each_child
+
+        project_venue_set = (self._project, self._venue)
+        # https://www.infoworld.com/article/3542595/6-python-libraries-for-parallel-processing.html
+        multithread_processor_props = MultiThreadProcessorProps(self._parallel_count)
+        multithread_processor_props.job_manager = JobManagerMemory(job_manager_props)
+        multithread_processor_props.job_executor = UploadItemExecutor(local_items, error_list, project_venue_set, self._staging_bucket, self._retry_wait_time_sec, self._retry_times, self._delete_files, True)
+        multithread_processor = MultiThreadProcessor(multithread_processor_props)
+        multithread_processor.start()
+
+        LOGGER.debug(f'finished dry-run uploading all granules')
+        while not local_items.empty():
+            results.append(local_items.get())
+
+        while not error_list.empty():
+            errors.append(error_list.get())
+
         if len(errors) > 0:
             print('There are ERRORS in the setup.', file=sys.stderr)
             for each in errors:
